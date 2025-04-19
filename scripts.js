7bfcf57aa827a00bc51e48cda6d23b6c8fe2bf65
SoulStreamzzCodezzz <zmhuff2012@gmail.com>
2025-04-18 22:02:03 -0400

    Create scripts.js
    Set the groundwork for the proxy and games buttons, urls are placeholders

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function navToGames() {
+  window.location.href = "https://example.com/" //this will change when a game.html file will be made
+}
+function navToProxy() {
+  window.location.href = "https://example.com/" //this will change when a proxy.html file will be made
+}
